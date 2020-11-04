8f976f51c29be18a6a0c0c988def80aa8b81ffd5
mikr96 <irsyadkamil96@gmail.com>
2020-11-04 15:46:49 +0800

    debugging

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 global.fetch = require("node-fetch");
 var indexRouter = require('./routes/index');
 var pagesRouter = require('./routes/pages');
 
 var app = express();
-
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
