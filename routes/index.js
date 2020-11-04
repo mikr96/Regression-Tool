8b0334108c03d41f278a6dd992bf9c699f30a475
mikr96 <irsyadkamil96@gmail.com>
2020-11-04 15:13:38 +0800

    tolong kerja el

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,22 @@ router.get('/result', function(req, res, next) {
   res.render('result');
 });
 
+router.get('/doctoroncall', async function(req, res, next) {
+  const url = 'https://cors-anywhere.herokuapp.com/https://www.doconcall.com.my/health-centre/api-campaign/duduk-rumah'
+  const data_await = await fetch(url);
+  const data = await data_await.json();
+
+  const finalize = [];
+  data.campaignElement.forEach(e => {
+      if(typeof e.campaignPromoTitle !== "undefined") {
+          finalize.push({
+              "campaignType": e.campaignType,
+              "campaignPromoTitle": e.campaignPromoTitle
+          })
+      }
+  })
+  console.log(finalize)
+  res.render('doctoroncall.ejs', {finalize: finalize});
+});
+
 module.exports = router;
