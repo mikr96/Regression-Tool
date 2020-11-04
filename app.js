39d44721df1c038d6b17a3456a8d6be086c63891
mikr96 <irsyadkamil96@gmail.com>
2020-11-04 15:23:30 +0800

    global fetch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fetch = require("node-fetch");
+global.fetch = require("node-fetch");
 var indexRouter = require('./routes/index');
 var pagesRouter = require('./routes/pages');
 
